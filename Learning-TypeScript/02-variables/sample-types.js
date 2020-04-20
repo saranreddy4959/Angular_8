33f3e37adbb304e9ed8f05da9278cd1dddbd9e9b
saran kumar reddy padala <saranreddy4959@gmail.com>
2020-04-20 16:04:38 -0400

    Learning typescript

---
+++ Learning-TypeScript/02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Saran";
+var lastName = "Padala";
+console.log(found);
+console.log("The grade is" + grade);
+console.log("Hi " + firstName + " " + lastName);
