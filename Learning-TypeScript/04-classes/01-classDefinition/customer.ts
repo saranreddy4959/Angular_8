4ca1d6c907b1b4224e0bffd3db7ad6a03e28b0ab
saran kumar reddy padala <saranreddy4959@gmail.com>
2020-04-23 19:41:38 -0400

    Learning TypeScript

---
+++ Learning-TypeScript/04-classes/01-classDefinition/customer.ts
@@ -0,0 +1,16 @@
+class customer {
+
+    firstName: string;
+    lastName: string;
+
+    constructor(theFirst: string, theLast: string) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
+    }
+}
+
+// let's create an instance
+let myCustomer = new customer("Martin", "Dixon");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
